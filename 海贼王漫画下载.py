ff17e01bacbd5e7c76c8d325b70542174dbf222d
阮程 <410093793@qq.com>
2019-01-02 15:30:04 +0800

    update

--- 海贼王漫画下载.py
+++ 海贼王漫画下载.py
@@ -4,13 +4,9 @@
 '''
 海贼王漫画下载程序:
 支持脚本传参或者手动指定下载剧集:
-例1:
+例如:
     python file_path p=1
     表示下载第一集, 下载剧集由参数 p 指定, 或者在脚本中修改 param_dict.get("p", 923) 中 param_dict 为空时的默认值
-例2:
-    python file_path p=10,11,12
-    表示下载第 10 11 12 三集
-
 @Author: rcddup
 @Email: 410093793@qq.com
 '''
@@ -23,7 +19,6 @@ import webbrowser
 import requests
 from PIL import Image
 
-cdir = os.path.dirname(os.path.abspath(__file__))
 
 def mkdirs(path=None, mode=0o777, exist_ok=True):
     '根据 path 创建目录'
@@ -79,15 +74,33 @@ def parse_arg_value(val):
 
 def parse_sys_argv():
     '解析参数'
-    print(sys.argv)
     if len(sys.argv) > 1:
         args = sys.argv[1]
         return {kv.split("=")[0]: parse_arg_value(kv.split("=")[1]) for kv in args.split("&")}
     else:
         return {}
 
-def produce(num):
-    temp_dir = cdir + "/temp/%s" % num
+def download(i):
+    url = base_url % i
+    resp = requests.get(url, timeout=5)
+    status = resp.status_code
+    if status == 404:
+        return
+    if resp.ok:
+        fp = "%s/%s.png" % (temp_dir, i)
+        pic_path_list.append(fp)
+        with open(fp, "wb") as f:
+            f.write(resp.content)
+            print(">>> " + fp)
+    else:
+        print("status[%s] text[%s]" % (status, resp.text()))
+
+
+if __name__ == '__main__':
+    param_dict = parse_sys_argv()
+    p = str(param_dict.get("p", 929))
+    cdir = os.path.dirname(os.path.abspath(__file__))
+    temp_dir = cdir + "/temp/%s" % p
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
     else:
@@ -95,36 +108,29 @@ def produce(num):
 
     pic_path_list = []
 
-    base_url = "http://img.17dm.com/op/manhua/" + num + "/%s.png"
+    base_url = "http://img.17dm.com/op/manhua/" + p + "/%s.%s"
 
-    # 下载漫画图片
-    for i in range(1, 100):
-        url = base_url % i
-        resp = requests.get(url, timeout=5)
-        status = resp.status_code
-        if status == 404:
-            break
-        if resp.ok:
-            fp = "%s/%s.png" % (temp_dir, i)
-            pic_path_list.append(fp)
-            with open(fp, "wb") as f:
-                f.write(resp.content)
-                print(">>> " + fp)
-        else:
-            print("status[%s] text[%s]" % (status, resp.text()))
+    pic_type_list = ["png", "jpg"]
 
-    comic_path = temp_dir + "/海贼王漫画%s.pdf" % num
-    # 生成 pdf
-    generator(comic_path, pic_path_list)
-    # 使用用户电脑默认浏览器打开 pdf
-    # webbrowser.open(comic_path)
+    for pic_type in pic_type_list:
+        for i in range(1, 100):
+            url = base_url % (i, pic_type)
+            resp = requests.get(url, timeout=5)
+            status = resp.status_code
+            if status == 404:
+                break
+            if resp.ok:
+                fp = "%s/%s.%s" % (temp_dir, i, pic_type)
+                pic_path_list.append(fp)
+                with open(fp, "wb") as f:
+                    f.write(resp.content)
+                    print(">>> " + fp)
+            else:
+                print("status[%s] text[%s]" % (status, resp.text()))
 
-
-if __name__ == '__main__':
-    param_dict = parse_sys_argv()
-    p = str(param_dict.get("p", 923))
-    ps = p.split(",")
-
-    for num in range(ps):
-        produce(num)
-
+    comic_path = temp_dir + "/海贼王漫画%s.pdf" % p
+    if len(pic_path_list) > 0:
+        # 生成 pdf
+        generator(comic_path, pic_path_list)
+        # 使用用户电脑默认浏览器打开 pdf
+        webbrowser.open(comic_path)
